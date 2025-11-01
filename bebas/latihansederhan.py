617837540a7e9c7b0d479a6083c70690985f3f2d
Pashya <58072770+pashya166@users.noreply.github.com>
2025-11-02 05:59:26 +0700

    Enhance temperature conversion program with additional scales

--- bebas/latihansederhan.py
+++ bebas/latihansederhan.py
@@ -1,7 +1,19 @@
-#latihan perhitungan sederhana
+#latihan perhitungan tempratur
 
 #program konvensi celcius satu sama lain
-print("\nProgram Konvensi Tempratur\n")
+print("\nProgam Konvensi Tempratur\n")
 
-celcius = float(input("Masukan Suhu Dalam Celcius: "))
-print("Suha adalah" ,celcius, "Celcius")
+celcius = float(input("Masukan Input suhu dalam celcius: "))
+print("suhu adalah", celcius, "Celcius")
+
+#reamur
+reamur = (4/5) * celcius
+print("suhu dalam reamur adalah", reamur, "Reamur")
+
+#fahrenheit
+fahrenheit = ((9/5) * celcius) + 32
+print("suhu dalam fahrenheit adalah", fahrenheit, "Fahrenheit")
+
+#kelvin
+kelvin = celcius + 273
+print("suhu dalam kelvin adalah", kelvin, "Kelvin")
